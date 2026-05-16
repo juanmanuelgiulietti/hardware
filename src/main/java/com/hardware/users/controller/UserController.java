5c70fa9e4caa532cc0eb12b0baf11f97598d5cb5
Juan Manuel Giulietti <juanmagiulietti04@gmail.com>
2026-05-16 16:55:51 -0300

    feat: add return response entity created

--- src/main/java/com/hardware/users/controller/UserController.java
+++ src/main/java/com/hardware/users/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.hardware.users.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +14,21 @@ import com.hardware.users.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-
-
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@Tag(name="Users", description="User endpoints")
+@Tag(name = "Users", description = "User endpoints")
 public class UserController {
+
     private final UserService userService;
 
     @PostMapping("/register")
-    public CreateUserResponse createUser(@RequestBody CreateUserRequest request) {
-        return userService.createUser(request);
+    public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest request) {
+        CreateUserResponse response = userService.createUser(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
-    
+
 }
